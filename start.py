f915a60ed524e0798a1f1a70eac9623d515c4179
ゼクロ <zekrommaster110@aol.de>
2018-02-12 19:52:15 +0100

    added switch between views and subs

--- start.py
+++ start.py
@@ -5,14 +5,18 @@ import sys
 
 subs = ''
 views = ''
-delta = 0
 time = 0
+s_delta = 0
 s_subs = 0
+v_delta = 0
+v_views = 0
 arg_user = None
 arg_interval = None
 debug = False
 display = True
 lcd = None
+
+status = 'subs'
 
 argv = sys.argv[1:]
 
@@ -37,7 +41,7 @@ with open('settings.json') as f:
     config = json.loads(f.read())
 
 def get_data():
-    global subs, views, s_subs, time
+    global subs, views, s_subs, time, s_delta, v_views, v_delta
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     url = 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername={}&key={}'.format(
         arg_user if arg_user != None else config['channel'], config['key']
@@ -50,7 +54,9 @@ def get_data():
 
     if (s_subs == 0):
         s_subs = subs
-    delta = int(subs) - int(s_subs)
+        v_views = views
+    s_delta = int(subs) - int(s_subs)
+    v_delta = int(views) - int(v_views)
     if (time > 86400 / config['interval']):
         time = 0
         s_subs = 0
@@ -72,24 +78,30 @@ def pointalize(string):
 print("INITIALIZED WITH CHANNEL: " + (arg_user if arg_user != None else config['channel']) + "\n")
 
 while (True):
-    def _delta():
+    def _delta(delta):
         return '+' + str(delta) if delta >= 0 else '-' + str(delta)
 
-    def _subs():
-        out = pointalize(subs) + ' subs'
-        for i in range(0, 16 - len(out) - len(str(delta)) - 1):
+    def _subs(mode):
+        out = pointalize(subs if mode == 'subs' else views) + ' ' + mode[:1]
+        for i in range(0, 16 - len(out) - len(str(s_delta if mode == 'subs' else v_delta)) - 1):
             out += ' '
-        out += _delta()
+        out += _delta(s_delta if mode == 'subs' else v_delta)
         return out
 
     get_data()
     # lcd.lcd_clear()
     if display:
-        lcd.lcd_display_string(_subs(), 1)
+        lcd.lcd_display_string(_subs(status), 1)
         lcd.lcd_display_string(strftime("%H:%M:%S", localtime()), 2)
     if debug:
         print(
-            _subs() + '\n' +
+            _subs(status) + '\n' +
             strftime("%H:%M:%S", localtime())
         )
-    sleep(arg_interval if arg_interval != None else config['interval'])
+
+    if status == 'subs':
+        status = 'views'
+    else:
+        status = 'subs'
+
+    sleep(int(arg_interval) if arg_interval != None else config['interval'])
